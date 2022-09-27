fc2ab6f6da22edb30916a0dfbc2bac5bbba85e06
Dmytro Kozhevin <dmytro@stellar.org>
2022-09-26 20:46:37 -0400

    Minor cleanup: group similar token functions together.

--- soroban-env-host/src/native_contract/token/contract.rs
+++ soroban-env-host/src/native_contract/token/contract.rs
@@ -72,15 +72,9 @@ pub trait TokenTrait {
         amount: BigInt,
     ) -> Result<(), Error>;
 
-    fn burn(
-        e: &Host,
-        admin: Signature,
-        nonce: BigInt,
-        from: Identifier,
-        amount: BigInt,
-    ) -> Result<(), Error>;
-
     fn freeze(e: &Host, admin: Signature, nonce: BigInt, id: Identifier) -> Result<(), Error>;
+
+    fn unfreeze(e: &Host, admin: Signature, nonce: BigInt, id: Identifier) -> Result<(), Error>;
 
     fn mint(
         e: &Host,
@@ -90,14 +84,20 @@ pub trait TokenTrait {
         amount: BigInt,
     ) -> Result<(), Error>;
 
+    fn burn(
+        e: &Host,
+        admin: Signature,
+        nonce: BigInt,
+        from: Identifier,
+        amount: BigInt,
+    ) -> Result<(), Error>;
+
     fn set_admin(
         e: &Host,
         admin: Signature,
         nonce: BigInt,
         new_admin: Identifier,
     ) -> Result<(), Error>;
-
-    fn unfreeze(e: &Host, admin: Signature, nonce: BigInt, id: Identifier) -> Result<(), Error>;
 
     fn decimals(e: &Host) -> Result<u32, Error>;
 
